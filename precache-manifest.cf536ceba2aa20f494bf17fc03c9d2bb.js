6f5bca15d030277beec0b5bc4cbdebc758627ddf
SGeorges <SGeorges@github.com>
2019-05-29 09:40:02 -0400

    Updates

---
+++ precache-manifest.cf536ceba2aa20f494bf17fc03c9d2bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9a4020d5aa8010135012e1914e4a8c",
+    "url": "/Click-e-Game/index.html"
+  },
+  {
+    "revision": "8e2f56c6fad7f4c6d5c9",
+    "url": "/Click-e-Game/static/css/main.df0a1d55.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Click-e-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8e2f56c6fad7f4c6d5c9",
+    "url": "/Click-e-Game/static/js/main.7dd4df46.chunk.js"
+  },
+  {
+    "revision": "5deed7be413592e74e62",
+    "url": "/Click-e-Game/static/js/runtime~main.495239c1.js"
+  }
+]);
